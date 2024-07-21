179a4f8f51f569d71d87a56f3f5465e2c0e21937
muruga21 <murugaperumal21062004@gmail.com>
2024-07-22 00:49:01 +0530

    created services for controllers

---
+++ services/generateUrl.service.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+interface Response {
+    error: boolean;
+    data: any | null;
+}
+
+const isLongUrlExists = async (longUrl: string): Promise<Response> => {
+    const response: Response = {
+        error: false,
+        data: null,
+    };
+    try {
+        const url = await prisma.url.findFirst({ where: { longUrl } });
+        response.data = url?.shortUrl;
+    } catch (err: any) {
+        response.error = true;
+        response.data = err.message;
+    }
+    return response;
+}
+
+const generateShortUrl = async (longUrl: string): Promise<string> => {
+    let shortUrl = Math.random().toString(36).substring(6);
+    while (await prisma.url.findFirst({ where: { shortUrl } })) {
+        shortUrl = Math.random().toString(36).substring(6);
+    }
+    return shortUrl;
+}
+
+export { isLongUrlExists, generateShortUrl };
