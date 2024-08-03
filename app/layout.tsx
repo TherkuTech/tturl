dfc2556e10cb5775924c947da4934c52cfa09515
MURUGA PERUMAL <112178257+muruga21@users.noreply.github.com>
2024-08-03 11:17:44 +0530

    Update layout.tsx - changed name to tturl

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "url-shortener",
+  title: "tturl",
   description: "shorten urls in one click",
 };
 
