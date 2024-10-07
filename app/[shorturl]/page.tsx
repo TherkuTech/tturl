8fa3d1ffcaabe0b17b7a91a18f907f96cb5029f4
muruga21 <murugaperumal21062004@gmail.com>
2024-10-07 09:43:36 +0530

    removed the unwanted commented code

--- app/[shorturl]/page.tsx
+++ app/[shorturl]/page.tsx
@@ -20,19 +20,19 @@ const Page = ({ params }: { params: { shorturl: string } }) => {
 
   const fetchUrl = async () => {
     try {
-      // const response = await axios.get(
-      //   `api/urlshortener?shortUrl=${params.shorturl}`
-      // );
-      // console.log(response.data.data);
-      // const url = response.data.data;
-      // if (url.startsWith("http://") || url.startsWith("https://")) {
-      //   window.location.href = url;
-      //   setLongUrl({ isValid: true, needToLoad: false });
-      //   return;
-      // } else {
-      //   window.location.href = `https://www.${url}`;
-      //   setLongUrl({ isValid: true, needToLoad: false });
-      // }
+      const response = await axios.get(
+        `api/urlshortener?shortUrl=${params.shorturl}`
+      );
+      console.log(response.data.data);
+      const url = response.data.data;
+      if (url.startsWith("http://") || url.startsWith("https://")) {
+        window.location.href = url;
+        setLongUrl({ isValid: true, needToLoad: false });
+        return;
+      } else {
+        window.location.href = `https://www.${url}`;
+        setLongUrl({ isValid: true, needToLoad: false });
+      }
     } catch (err: any) {
       console.log(err.message);
       setLongUrl({ isValid: false, needToLoad: false });
