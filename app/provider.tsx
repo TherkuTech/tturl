bb1fc520594e4e9db4e1e5eaf04186ece902a271
muruga21 <murugaperumal21062004@gmail.com>
2024-10-17 19:59:49 +0530

    deleted provided tsx

--- app/provider.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import {ThemeProvider as NextThemesProvider} from "next-themes";
-export default function ThemeProvider({children}: {children: React.ReactNode}, ...props: any[]) {
-  return <NextThemesProvider {...props}>
-    {children}
-    </NextThemesProvider>;
-}
