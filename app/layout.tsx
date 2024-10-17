79768eb9dcb6711e647566eda798360a7add6df8
muruga21 <murugaperumal21062004@gmail.com>
2024-10-17 21:17:59 +0530

    fixing errors before production

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
-import ThemeProvider from "./provider";
-import bg from '../assets/bg.svg'
 
 const inter = Inter({ subsets: ["latin"] });
 
