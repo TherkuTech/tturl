60dd172756127879efed8f2c05903c748bf8e20a
Dakshata <dakshatamishralakshya@gmail.com>
2024-10-18 09:59:53 +0530

    faq section added

---
+++ app/components/faqaccordian.jsx/faqpg.js
@@ -0,0 +1,16 @@
+const accordian = document.querySelectorAll('.accordian');
+
+accordian.forEach((accordian) => {
+    const icon = accordian.querySelector('.icon');
+    const answer = accordian.querySelector('.answer');
+
+    accordian.addEventListener('click', () => {
+        if (icon.classList.contains('active')) {
+            icon.classList.remove('active');
+            answer.style.maxHeight = null;
+        } else {
+            icon.classList.add('active');
+            answer.style.maxHeight = answer.scrollHeight + 'px';
+        }
+    });
+});
